012474cf7319e8c14126122f0d7ea2601f4469e5
Raphi1001 <rwertitsch@yahoo.com>
2021-12-07 18:42:55 +0100

    docker tschaubaba

--- _build.bat
+++
@@ -1,8 +0,0 @@
-@cls
-@TITLE C Sample Client/Server
-
-docker stop cs-sample-instance
-docker rm   cs-sample-instance
-
-docker build -t cs-sample-container:latest .
-docker run --rm -ti --privileged -v c:\repos\ClientServerSampleInC\src\:/src:rw -p 6543:6543/tcp --name cs-sample-instance cs-sample-container
--- _interact.bat
+++
@@ -1,1 +0,0 @@
-docker exec -it cs-sample-instance bash
